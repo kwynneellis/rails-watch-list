3b0aef475347ec683f359fafc9b8d8dd6177ad6e
Kat WynneEllis <k.wynneellis@gmail.com>
2023-02-23 14:47:25 +0000

    functionality is working. missing the styling, but I'm off home to get healthy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :lists, only: %i[index show create new] do
+    resources :bookmarks, only: %i[create new]
+  end
+  resources :bookmarks, only: %i[destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
